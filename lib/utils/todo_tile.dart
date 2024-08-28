7fbb1e8d4466b4d29e1b4a88345bc06dc920e302
kenier <kenier202@gmail.com>
2024-08-28 15:07:35 -0500

    feat(TodoTile): add main cards to do

---
+++ lib/utils/todo_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TodoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskComplete;
+  final Function(bool?)? onChange;
+
+  const TodoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskComplete,
+      this.onChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, top: 25, right: 25, bottom: 0),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: taskComplete,
+              onChanged: onChange,
+              activeColor: Colors.black,
+            ),
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: taskComplete
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
